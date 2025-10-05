e611a3b69f1b08df42329101e4f3c3eb23cbebd0
Alex Sharikov <alex@controlx.au>
2025-10-06 09:18:13 +1100

    refactor: housekeeping

--- examples/original/getAttributeSingle.ts
+++ examples/original/getAttributeSingle.ts
@@ -1,16 +1,16 @@
 import { Buffer } from "node:buffer";
 import { Controller } from "../../src/main.ts";
 
-let c = new Controller(false);
+const plc = new Controller(false);
 
 // Ethernet IP Device Get Parameter (reading name of PLC 1769-L32E)
-c.connect("192.168.111.11", Buffer.from([]), false).then(async () => {
+plc.connect("192.168.111.11", Buffer.from([]), false).then(async () => {
   // Get parameter value:
   // Class ID 0x01 (Identity) with Instance 0x01 and read the "Name" Attribute (0x07)
-  const value = await c.getAttributeSingle(0x01, 0x01, 0x07).catch((e) => {
+  const value = await plc.getAttributeSingle(0x01, 0x01, 0x07).catch((e) => {
     console.log(e);
   });
   console.log(value?.toString());
 
-  c.disconnect();
+  plc.disconnect();
 });
